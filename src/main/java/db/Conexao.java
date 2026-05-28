6cb55c8f44d338d520d96f004525ca986a1cf9f7
becunha7 <becunha7@gmail.com>
2026-05-28 16:23:55 -0300

    feat: adicionado informações para entrar no banco de dados

--- src/main/java/db/Conexao.java
+++ src/main/java/db/Conexao.java
@@ -3,5 +3,8 @@ package db;
 
 
 public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/controle_estoque";
+    private static final String USER = "root";
+    private static final String PASS = "root";
     
 }
