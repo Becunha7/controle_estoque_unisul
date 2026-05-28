07d278966de26db1ff8893aeb1b86df0fba711cf
becunha7 <becunha7@gmail.com>
2026-05-28 18:25:25 -0300

    feat:Implementa o método de cadastro em CategoriaDAO.

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
+import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
 import modelo.Categoria;
-import db.Conexao; 
+import db.Conexao;
 
 public class CategoriaDAO {
 
@@ -11,5 +13,18 @@ public class CategoriaDAO {
         Connection con = Conexao.getConnection();
         PreparedStatement stmt = null;
         String sql = "INSERT INTO categoria (nome, tamanho, embalagem) VALUES (?, ?, ?)";
-    } 
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, categoria.getNome());
+            stmt.setString(2, categoria.getTamanho().name().toUpperCase());
+            stmt.setString(3, categoria.getEmbalagem().name().toUpperCase());
+
+            stmt.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Categoria salva com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar: " + ex.getMessage());
+        } finally {
+            Conexao.closeConnection(con, stmt); 
+        }
+    }
 }
--- src/main/java/db/Conexao.java
+++ src/main/java/db/Conexao.java
@@ -1,10 +1,42 @@
-
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Conexao {
+
     private static final String URL = "jdbc:mysql://localhost:3306/controle_estoque";
     private static final String USER = "root";
     private static final String PASS = "root";
+
     
+    public static Connection getConnection() {
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+           
+            return DriverManager.getConnection(URL, USER, PASS);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+            System.out.println("Conexão fechada com sucesso!");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao fechar conexão: " + ex.getMessage());
+        }
+    }
 }
