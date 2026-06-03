a2e0959c7425c317d80de14bab08fbeea991dcff
becunha7 <becunha7@gmail.com>
2026-06-02 22:05:08 -0300

    fix:url do MySQL

--- src/main/java/db/Conexao.java
+++ src/main/java/db/Conexao.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/controle_estoque";
+    private static final String URL = "jdbc:mysql://localhost:3306/controle_estoque_unisul";
     private static final String USER = "root";
     private static final String PASS = "root";
 
